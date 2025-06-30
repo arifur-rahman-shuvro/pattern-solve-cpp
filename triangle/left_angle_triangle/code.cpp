e3dd96c833b4579baa0d371f0ec715db376405bc
arifur-rahman-shuvro <arifurrahmanshuvroars@gmail.com>
2025-06-30 14:24:40 +0600

    Left aligned pattern solved

--- triangle/left_angle_triangle/code.cpp
+++ triangle/left_angle_triangle/code.cpp
@@ -2,7 +2,7 @@
 #include <chrono>
 #include<thread>
 
-void right_aligned_triangle(int row){
+void left_aligned_triangle(int row){
     for(int i=1; i<=row; i++){
         for(int j=1; j<=i; j++){
             std::cout << " *";
@@ -20,16 +20,11 @@ int main(){
     std::cout << "Welcome to PATTERN GENERATOR" <<std::endl;
     do{ std::cout << "Enter number of rows(numeric): ";
     std::cin >> num_of_rows;
-    if(num_of_rows <= 0){
-            std::cout << "Please enter a positive number.\n";
-            continue;
-        }
-    
-    
+  
     std::cout << "Pattern Generating......" << std::endl;
     std::cout << std::endl;
 
-    right_aligned_triangle(num_of_rows);
+    left_aligned_triangle(num_of_rows);
     std::cout << "\n";
     
     std::cout << "Pattern Generated" << std::endl;
