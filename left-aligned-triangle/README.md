6ffc0faa209b717e70f1368100337b0af1f71c88
arifur-rahman-shuvro <arifurrahmanshuvroars@gmail.com>
2025-07-01 13:22:58 +0600

    Left Aligned Triangle pattern Solved

---
+++ left-aligned-triangle/README.md
@@ -0,0 +1,28 @@
+# FILE OVERVIEW
+
+This file contain "LEFT ALIGNED TRIANGLE" pattern solve.
+
+## Pattern overview
+
+Have to print a left aligned triangle using starts(*).
+
+## I/O example
+
+Input: 5
+Outout: *
+        **
+        ***
+        ****
+        *****
+
+## Hint
+
+- Use nested loop.
+- Outer loop for row count
+- Inner loop for print star in every rows
+
+
+
+
+### Thanks for visiting
+Feel free to explore more patterns, contribute
---
+++ left-aligned-triangle/code.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// Main function
+void left_aligned_triangle(int row){
+    for(int i=1; i<=row; i++){  // outer loop for row count
+        for(int j=1; j<=i; j++){ // inner loop for print star
+            cout << " *";
+        }
+        cout << "\n";
+    }
+}
+
+
+
+int main(){
+    int num_of_rows;
+    
+    cout << "Enter row number: ";
+    cin >> num_of_rows;
+  
+    left_aligned_triangle(num_of_rows);
+    
+    return 0;
+   
+
+}
