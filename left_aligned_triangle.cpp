70c9bc597cbd6a59c3ccfe6b65f5cb65b960d5b0
arifur-rahman-shuvro <arifurrahmanshuvroars@gmail.com>
2025-07-02 10:24:43 +0600

    Updated structure

--- left_aligned_triangle.cpp
+++ left_aligned_triangle.cpp
@@ -1,7 +1,13 @@
+/*
+Author: Arifur Rahman Shuvro
+Date:01 July 2025
+GitHub: https://github.com/arifur-rahman-shuvro
+*/
+
 #include <iostream>
 using namespace std;
 
-// Main function
+// Root function
 void left_aligned_triangle(int row){
     for(int i=1; i<=row; i++){  // outer loop for row count
         for(int j=1; j<=i; j++){ // inner loop for print star
